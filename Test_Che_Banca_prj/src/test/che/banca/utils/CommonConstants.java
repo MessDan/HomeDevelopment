ffd074962914d7b80f8e694772b58bfd14bb4d22
MessDan <43014247+MessDan@users.noreply.github.com>
2019-12-30 01:54:06 +0100

    delete TestCHB

--- Test_Che_Banca_prj/src/test/che/banca/utils/CommonConstants.java
+++
@@ -1,8 +0,0 @@
-package test.che.banca.utils;
-
-public class CommonConstants {
-
-	public static String SPACE_SEPARATOR = " ";
-	public static String LINE_SEPARATOR = " - ";
-	public static String SEMICOLON_SEPARATOR = ";";
-}
